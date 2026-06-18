779783bc4aa0d32adfc306b2c783bbefd2e06a2d
WellintonBao <wellintonbao@gmail.com>
2026-06-18 09:45:21 -0300

    esqueci o nome

--- src/aluno/models.py
+++ src/aluno/models.py
@@ -5,8 +5,13 @@ class Aluno(models.Model):
     matricula = models.CharField(
         primary_key=True,
         max_length=20)
-
+    
     num_telefone = models.CharField(
+        max_length=20,
+        unique=True
+    )
+
+    nmaluno = models.CharField(
         max_length=20,
         unique=True
     )
